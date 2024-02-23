45a8415dcdd82cdf47b7abeae4d37397d6dc89b9
Ashton Michelstein <aam1727@g.rit.edu>
2024-02-22 22:58:53 -0500

    front page

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
+import schemas from './sanity/schemas'
 
 const config = defineConfig({
     projectId: "54xh2kz9",
@@ -7,7 +8,8 @@ const config = defineConfig({
     title: "Personal Website",
     apiVersion: "2024-02-20",
     basePath: "/admin",
-    plugins: [structureTool()]
+    plugins: [structureTool()],
+    schema: {types: schemas}
 })
 
 export default config
---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,22 @@
+import { Project } from "@/types/Projects";
+import { createClient, groq } from "next-sanity";
+
+export async function getProjects(): Promise<Project[]> {
+    const client = createClient({
+        projectId: "54xh2kz9",
+        dataset: "production",
+        apiVersion: "2024-02-20",
+    })
+
+    return client.fetch(
+        groq`*[_type == "project"]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            content
+        }`
+    )
+}
