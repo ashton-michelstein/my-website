004693bb5ed70bedb04c3e2acaabd3ef3d1c3484
Ashton Michelstein <aam1727@g.rit.edu>
2024-02-27 19:10:23 -0500

    changed text in grid

--- app/page.tsx
+++ app/page.tsx
@@ -21,16 +21,16 @@ export default async function Home() {
           <h1 className="text-7xl font-extrabold text-white"> Hello, I'm Ashton </h1>
           <p className="mt-3 text-xl text-white"> description description description description </p>
           <h2 className="mt-24 font-bold text-white text-3xl"> My Projects ⬇️ </h2>
-          <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {projects.map((project: Project) => (
               <Link
                 href={`/projects/${project.slug}`}
                 key={project._id} className="border-2 border-neutral-700 rounded-lg p-1 hover:scale-105 hover:border-white transition">
                 <div className= "flow-root">
-                  <div className ="float-left ml-4 mt-4 font-semibold text-white"> {project.name} </div>
-                  <div className ="float-right mr-4 mt-4 font-semibold text-indigo-300"> {project.date} </div>
+                  <div className ="float-left ml-4 mt-4 text-lg font-semibold text-white"> {project.name} </div>
+                  <div className ="float-right mr-4 mt-4 text-lg font-semibold text-indigo-300"> {project.date} </div>
                 </div>
-                <div className="mt-3 ml-4 mr-4 mb-4 text-sm text-white"> {description(project)} </div>
+                <div className="mt-3 ml-4 mr-4 mb-4 text-base text-white"> {description(project)} </div>
               </Link>
             ))}
           </div>
