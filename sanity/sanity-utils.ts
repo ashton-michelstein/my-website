66f67eddce95d5e96aa4373ef47aa86ea82b0f33
Ashton Michelstein <aam1727@g.rit.edu>
2024-02-28 18:38:16 -0500

    project page

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -21,3 +21,25 @@ export async function getProjects(): Promise<Project[]> {
         }`
     )
 }
+
+export async function getProject(slug: string): Promise<Project> {
+    const client = createClient({
+        projectId: "54xh2kz9",
+        dataset: "production",
+        apiVersion: "2024-02-20",
+    })
+
+    return client.fetch(
+        groq`*[_type == "project" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url,
+            date,
+            content
+        }`,
+        {slug}
+    )
+}
